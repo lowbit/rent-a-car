98b4061abcdf00e34fdb208e710694d94178cde2
Rijad <spaha92@gmail.com>
2020-08-06 19:46:09 +0200

    Add Edit Delete Vijesti form and popup confirmation dialog/modal

--- Rentacar.Web/Controllers/VijestiController.cs
+++ Rentacar.Web/Controllers/VijestiController.cs
@@ -13,7 +13,7 @@ namespace Rentacar.Web.Controllers
     public class VijestiController : Controller
     {
         private MyContext _context;
-        public VijestiController (MyContext context)
+        public VijestiController(MyContext context)
         {
             _context = context;
         }
@@ -23,17 +23,102 @@ namespace Rentacar.Web.Controllers
 
             var vijesti = from v in _context.Vijestis select v;
             int pageSize = 5;
-            return View("VijestiList", await PaginatedList<Vijesti>.CreateAsync(vijesti.AsNoTracking(), pageNumber ?? 1, pageSize));
+            return View("VijestiList", await PaginatedList<Vijesti>.CreateAsync(vijesti.AsNoTracking().OrderByDescending(v => v.Datum_i_vrijeme_objave), pageNumber ?? 1, pageSize));
             //return View(await _context.Vijestis.ToListAsync());
         }
-        public async Task<IActionResult> Details(
-            int? id)
+        public async Task<IActionResult> Details(int? id)
         {
-            var vijest = await _context.Vijestis.FindAsync(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var vijest = await _context.Vijestis.Include(a => a.Autor).ThenInclude(k=>k.Korisnik).AsNoTracking().FirstOrDefaultAsync(v => v.Id == id);
+            if (vijest == null)
+                return NotFound();
             vijest.Ukupno_pregleda++;
             _context.Entry(vijest).CurrentValues.SetValues(vijest);
             _context.SaveChanges();
             return View("VijestiDetails", vijest);
+        }
+        public ActionResult AddEdit(int? id)
+        {
+            if(id == null)
+                return View("VijestiAddEdit");
+            return View("VijestiAddEdit", _context.Vijestis.FirstOrDefault(v=> v.Id == id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add([Bind("Naslov, Slika, URL, Sadrzaj")] Vijesti model)
+        {
+            //Dodati logovanog korisnika kao autora i postaviti broj pregleda na 0
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    model.Ukupno_pregleda = 0;
+                    model.Datum_i_vrijeme_objave = DateTime.Now;
+                    //Until login system implemented correctly
+                    var kNalog = _context.Korisnicki_nalogs.FirstOrDefaultAsync();
+                    model.AutorId = kNalog.Result.Id;
+
+                    _context.Add(model);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { id = model.Id });
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View("VijestiAddEdit", model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind("Id, Naslov, Slika, URL, Sadrzaj")] Vijesti model)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    model.Datum_i_vrijeme_objave = DateTime.Now;
+                    //Until login system implemented correctly
+                    var kNalog = _context.Korisnicki_nalogs.FirstOrDefaultAsync();
+                    model.AutorId = kNalog.Result.Id;
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { id = model.Id });
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View("VijestiAddEdit", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Vijesti vijest = new Vijesti() { Id = id };
+                _context.Entry(vijest).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                //Log error once logging implemented
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 }
--- Rentacar.Web/wwwroot/js/site.js
+++ Rentacar.Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Delete Confirmation Modal
+// Get the modal
+var modal = document.getElementById('deletemodal');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
